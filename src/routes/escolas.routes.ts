88ce227f24f552bbbb49e06030ed99c7b53eb286
Guilherme Bento <guilherme1550@hotmail.com>
2021-03-25 22:32:04 -0300

    branch develop

--- src/routes/escolas.routes.ts
+++ src/routes/escolas.routes.ts
@@ -5,7 +5,7 @@ const escolasRoutes = Router();
 escolasRoutes.get("/", (request, response) => {
   return response.json({
     nome: "Escola Teste",
-    endereço: "Endereço Teste",
+    endereco: "Endereço Teste",
     pontos: 10,
     parceira: true,
   });
