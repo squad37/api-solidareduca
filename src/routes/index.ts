26e452ac5070ea25e15e7931570d003b02d90cb3
JeanHiga <jeaneduardohiga@gmail.com>
2021-03-28 10:12:21 -0300

    Inclusão rota get material

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { escolasRoutes } from "./escolas.routes";
+import { materialRoutes } from "./material.routes";
 
 const router = Router();
 
 router.use("/escolas", escolasRoutes);
+router.use("/material", materialRoutes);
 
 export { router };
