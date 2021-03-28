f645f46dbcf9f2781f79d13cbce03a0e7e1c6f17
JeanHiga <jeaneduardohiga@gmail.com>
2021-03-28 16:27:44 -0300

    Inclusão da rota material

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { alunosRoutes } from "./alunos.routes";
 import { escolasRoutes } from "./escolas.routes";
+import { materialRoutes } from "./material.routes";
 
 const router = Router();
 
 router.use("/escolas", escolasRoutes);
 router.use("/alunos", alunosRoutes);
+router.use("/material", materialRoutes);
 
 export { router };
