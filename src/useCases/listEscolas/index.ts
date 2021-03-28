e66788489b27aa74fc8524276aea7b57c2cdd616
JeanHiga <jeaneduardohiga@gmail.com>
2021-03-28 10:19:32 -0300

    Create index.ts

---
+++ src/useCases/listEscolas/index.ts
@@ -0,0 +1,15 @@
+import { EscolasRepository } from "../../repositories/EscolasRepository";
+import { ListEscolasController } from "./ListEscolasController";
+import { ListEscolasUseCase } from "./ListEscolasUseCase";
+
+
+
+export default (): ListEscolasController => {
+  const escolasRepository = new EscolasRepository();
+  const listEscolasUseCase = new ListEscolasUseCase(escolasRepository);
+  const listEscolasController = new ListEscolasController(listEscolasUseCase);
+
+  return listEscolasController;
+};
+
+
