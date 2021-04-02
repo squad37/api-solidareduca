35840340403d3a4fd9b58c901a4c39b2f5a33f54
JeanHiga <jeaneduardohiga@gmail.com>
2021-04-02 16:53:31 -0300

    Update index.ts

--- src/useCases/listEscolasByNome/index.ts
+++ src/useCases/listEscolasByNome/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { EscolasRepository } from "../../repositories/EscolasRepository";
 import { ListEscolasByNomeController } from "./ListEscolasByNomeController";
 import { ListEscolasByNomeUseCase } from "./ListEscolasByNomeUseCase";
@@ -13,20 +12,4 @@ export default (): ListEscolasByNomeController => {
   );
 
   return listEscolasByNomeController;
-=======
-import { EscolasRepository } from "../../repositories/EscolasRepository";
-import { ListEscolasByNomeController } from "./ListEscolasByNomeController";
-import { ListEscolasByNomeUseCase } from "./ListEscolasByNomeUseCase";
-
-export default (): ListEscolasByNomeController => {
-  const escolasRepository = new EscolasRepository();
-  const listEscolasByNomeUseCase = new ListEscolasByNomeUseCase(
-    escolasRepository
-  );
-  const listEscolasByNomeController = new ListEscolasByNomeController(
-    listEscolasByNomeUseCase
-  );
-
-  return listEscolasByNomeController;
->>>>>>> a07737ab6d06961dc6053869aa593f0252b14d51
 };
